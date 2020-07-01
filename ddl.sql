02999c8fd3f7fc415c5192783a17e4dc5e43f2e9
andrea <andreatwelve@sina.cn>
2020-07-01 18:21:27 +0800

    add ddl.sql and dml.sql

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use student_examination_sys;
 
 -- Create a database
+create database if not exists test;
 
 -- Create the database of the designated character set
+create database uncode character set utf8;
 
 -- Display the creation information fo the database
+show create database uncode;
 
 -- Revise the codes of the database
+alter database uncode character set gb2312;
 
 -- Delete a database
+drop database uncode;
 
 -- **Table level**
 -- Revise table name
+alter table score rename score_a; 
 
 -- Revise the field's data type
+alter table score_a modify score decimal(6,1);
 
 -- Revise field name
+alter table score_a change score myscore decimal(4,1);
 
 -- Add field
+alter table score_a add ranking int;
 
 -- Delete field
+alter table score_a drop ranking; 
 
 -- Revise the table's storage engine
+alter table score_a engine=MyISAM;
 
 -- Delete the table's foreign key restriant
+alter table score_a drop foreign key FK_STUDENT_ID;
 
 -- Delete a table
+drop table score_a;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,14 @@
 -- Insert record
+insert into student 
+values
+('001', 'Colin', 18, 'male'),
+('002', 'Emma', 20, 'female');
 
 -- Revise record
+update student set id = '004' where id = '002';
 
 --  Delete record
+delete from student where id = '004';
 
 -- Search record
+select * from student where id = '001';
